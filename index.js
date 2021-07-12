460416490ca5817e4f7327d30c17c0452f694f99
edneymoita <edneymoita@gmail.com>
2021-07-12 19:10:41 -0300

    Versao inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logoPretha from '../components/logoPretha';
+import logoPretha from '../assets/image/logo/logo-pretha.png';
+import Title from '../components/title';
+import title from '../components/title'
+
+// console.log({logoPretha});
+
+function Home(){
+    return (
+        <div>
+            <Title>Home</Title>
+            {/* <img src={logoPretha}/> */}
+        </div>
+    );
+}
+
+export default Home;
