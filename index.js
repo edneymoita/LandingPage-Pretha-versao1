a19c69cee7526f0a3c67a740a04f94ba87685e50
edneymoita <edneymoita@gmail.com>
2021-07-13 17:19:05 -0300

    Mensagem de erro e uso de localStorage

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
 function validateEmail(){
+    let emailValue = '';
+    
+    emailValue = document.getElementById('emailBox').value;
+
+    if (emailValue.includes('@')){
+        localStorage.clear();
+        localStorage.setItem('email', emailValue);
+        document.getElementById('msg').innerHTML = `Presente enviado para o e-mail <i>${emailValue}</i>.</br>Aproveite bem!`;
+    } else {
+        document.getElementById('msg').value = 'E-mail inválido! Favor verificar.';
+        alert(document.getElementById('msg').value);
+    }
     return true;
 }
