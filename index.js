7a16756ec1a7c0d1aa85e1d4fe6e3d37ef1c46ec
edneymoita <edneymoita@gmail.com>
2021-07-12 21:33:17 -0300

    Ajuste de texto e inclusão de imagens

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-import React from 'react';
-//import logoPretha from '../components/logoPretha';
-import logoPretha from '../assets/image/logo/logo-pretha.png';
-import Title from '../components/title';
-import title from '../components/title'
 
-// console.log({logoPretha});
-
-function Home(){
-    return (
-        <div>
-            <Title>Home</Title>
-            {/* <img src={logoPretha}/> */}
-        </div>
-    );
+function validateEmail(){
+    return true;
 }
-
-export default Home;
